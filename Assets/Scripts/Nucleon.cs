d16c40b23ae41d3b7e91c2bc04b1ed39ea44bc7b
dgoldaraz <dgoldaraz@gmail.com>
2017-11-15 23:17:41 +0000

    Add basic molecule system

---
+++ Assets/Scripts/Nucleon.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Nucleon : MonoBehaviour {
+
+
+    public Rigidbody Body;
+    public float AttractionForce = 10.0f;
+
+    void Awake()
+    {
+        Body = GetComponent<Rigidbody>();
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+		//Apply attraction to the center of the scene (we are applying our poisition as a negative force)
+        if(Body)
+        {
+            Body.AddForce(transform.localPosition * -AttractionForce);
+        }
+	}
+}
---
+++ Assets/Scripts/NucleonSpawner.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NucleonSpawner : MonoBehaviour {
+
+    public float TimeBetweenSpawns;
+
+    public float SpawnDistance;
+
+    public Nucleon[] NucleonPrefabs;
+
+    float TimeSinceLastSpawn;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        TimeSinceLastSpawn += Time.deltaTime;
+        if(TimeSinceLastSpawn > TimeBetweenSpawns)
+        {
+            TimeSinceLastSpawn = 0.0f;
+            SpawnNucleon();
+        }
+	}
+
+    void SpawnNucleon()
+    {
+        Nucleon Prefab = NucleonPrefabs[Random.Range(0, NucleonPrefabs.Length)];
+        Nucleon SpawnN =  Instantiate<Nucleon>(Prefab);
+        SpawnN.transform.localPosition = Random.onUnitSphere * SpawnDistance;
+    }
+}
